0e3e5a299b5115c292e2ee0ffd95acc8521456fa
Lukas Rupprecht <Lukas.Rupprecht@ibm.com>
2020-03-31 15:46:16 -0700

    adds config

---
+++ collection-system/util/config.cpp
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2020 IBM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "config.h"
+#include "logger.h"
+
+#include <fstream>
+
+config_opts_t Config::config;
+
+/**
+ * Parses the config file at the provided path. The config
+ * format is expected to be a simple 'key = value' format.
+ * Comments are prefixed by '#'.
+ */
+int Config::parse_config(std::string path) {
+  std::ifstream config_file(path);
+  std::string line;
+
+  while (std::getline(config_file, line)) {
+    // ignore comments
+    if (line.at(0) == '#') continue;
+
+    size_t pos = line.find("=");
+    // ignore invalid lines
+    if (pos == std::string::npos) {
+      LOG_ERROR("\"" << line << "\" is an invalid config entry, ignoring it.");
+      continue;
+    }
+
+    // get and trim key and value
+    std::string key = line.substr(0, pos);
+    key.erase(key.begin(), std::find_if(key.begin(), key.end(), [](int c) {
+      return !std::isspace(c);
+    }));
+    key.erase(std::find_if(key.rbegin(), key.rend(), [](int c) {
+      return !std::isspace(c);
+    }).base(), key.end());
+
+    std::string val = line.substr(pos);
+    val.erase(val.begin(), std::find_if(val.begin(), val.end(), [](int c) {
+      return !std::isspace(c);
+    }));
+    val.erase(std::find_if(val.rbegin(), val.rend(), [](int c) {
+      return !std::isspace(c);
+    }).base(), val.end());
+
+    Config::config[key] = val;
+  }
+
+  return 0;
+}
+
+
