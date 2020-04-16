90c1f2dde9c11ed8b97402021440cbe25986c01c
Lukas Rupprecht <Lukas.Rupprecht@ibm.com>
2020-04-16 16:56:39 -0700

    adds KafkaInputStream

---
+++ collection-system/io/kafka-input-stream.cpp
@@ -0,0 +1,143 @@
+/**
+ * Copyright 2020 IBM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "kafka-input-stream.h"
+#include "logger.h"
+#include "error.h"
+#include "config.h"
+
+KafkaInputStream::KafkaInputStream(std::string t, std::string b, std::string g) :
+    topic(t), brokers(b), group_id(g) {
+  conf = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
+  consumer = nullptr;
+}
+
+KafkaInputStream::~KafkaInputStream() {
+  if (conf) delete conf;
+  if (consumer) delete consumer;
+}
+
+int KafkaInputStream::open() {
+  std::string errstr;
+
+  // set brokers
+  if (conf->set("metadata.broker.list", brokers, errstr) != RdKafka::Conf::CONF_OK) {
+    LOG_ERROR("Couldn't set brokers for KafkaInputStream: " << errstr);
+    return ERROR_NO_RETRY;
+  }
+  // set group ID
+  if (conf->set("group.id", brokers, errstr) != RdKafka::Conf::CONF_OK) {
+    LOG_ERROR("Couldn't set group ID for KafkaInputStream: " << errstr);
+    return ERROR_NO_RETRY;
+  }
+  // enable auto commit
+  if (conf->set("enable.auto.commit", "true", errstr) != RdKafka::Conf::CONF_OK) {
+    LOG_ERROR("Couldn't enable auto commit for KafkaInputStream: " << errstr);
+    return ERROR_NO_RETRY;
+  }
+  // set 60s session timeout
+  if (conf->set("session.timeout.ms", "60000", errstr) != RdKafka::Conf::CONF_OK) {
+    LOG_ERROR("Couldn't set session timout for KafkaInputStream: " << errstr);
+    return ERROR_NO_RETRY;
+  }
+  // set SASL authentication options
+  if (!Config::config[Config::CKEY_KAFKA_SASL_USER].empty() &&
+      !Config::config[Config::CKEY_KAFKA_SASL_PASS].empty()) {
+    if (conf->set("security.protocol", "sasl_plaintext", errstr) != RdKafka::Conf::CONF_OK) {
+      LOG_ERROR("Failed to set security protocol for KafkaInputStream: " << errstr);
+      return ERROR_NO_RETRY;
+    }
+    if (conf->set("sasl.mechanisms", "SCRAM-SHA-512", errstr) != RdKafka::Conf::CONF_OK) {
+      LOG_ERROR("Failed to set sasl mechanism for KafkaInputStream: " << errstr);
+      return ERROR_NO_RETRY;
+    }
+    if (conf->set("sasl.username", Config::config[Config::CKEY_KAFKA_SASL_USER],
+        errstr) != RdKafka::Conf::CONF_OK) {
+      LOG_ERROR("Failed to set sasl username for KafkaInputStream: " << errstr);
+      return ERROR_NO_RETRY;
+    }
+    if (conf->set("sasl.password", Config::config[Config::CKEY_KAFKA_SASL_PASS],
+        errstr) != RdKafka::Conf::CONF_OK) {
+      LOG_ERROR("Failed to set sasl password for KafkaInputStream: " << errstr);
+      return ERROR_NO_RETRY;
+    }
+  }
+
+  // create consumer
+  consumer = RdKafka::KafkaConsumer::create(conf, errstr);
+  if (!consumer) {
+    LOG_ERROR("Couldn't create Kafka consumer: " << errstr);
+    return ERROR_NO_RETRY;
+  }
+
+  // subscribe to topic
+  RdKafka::ErrorCode err = consumer->subscribe({ topic });
+  if (err) {
+    LOG_ERROR("Couldn't subscribe to topic " << topic << ": " << RdKafka::err2str(err));
+    return ERROR_NO_RETRY;
+  }
+
+  return NO_ERROR;
+}
+
+void KafkaInputStream::close() {
+  consumer->close();
+}
+
+int KafkaInputStream::recv(std::string &next_msg) {
+  RdKafka::Message *msg;
+  bool done = false;
+  int rc;
+
+  // get next message from Kafka
+  consumer->consume(TIMEOUT_MS);
+  while (!done) {
+    switch (msg->err()) {
+    case RdKafka::ERR__TIMED_OUT:
+      // try again
+      break;
+    case RdKafka::ERR_NO_ERROR:
+      if (static_cast<int>(msg->len()) > 0) {
+        next_msg = static_cast<const char*>(msg->payload());
+        done = true;
+        rc = NO_ERROR;
+      } else {
+        // try again
+        LOG_WARN("Received empty message.");
+      }
+      break;
+    case RdKafka::ERR__PARTITION_EOF:
+      LOG_INFO("Reached EOF.");
+      done = true;
+      rc = ERROR_EOF;
+      break;
+    case RdKafka::ERR__UNKNOWN_TOPIC:
+    case RdKafka::ERR__UNKNOWN_PARTITION:
+      LOG_ERROR("Consume failed with: " << msg->errstr());
+      rc = ERROR_RETRY;
+      done = true;
+      break;
+    default:
+      LOG_ERROR("Consume failed with: " << msg->errstr());
+      rc = ERROR_RETRY;
+      done = true;
+      break;
+    }
+  }
+
+  delete msg;
+  return rc;
+}
---
+++ collection-system/io/kafka-input-stream.h
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2020 IBM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IO_KAFKA_INPUT_STREAM_H_
+#define IO_KAFKA_INPUT_STREAM_H_
+
+#include "msg-input-stream.h"
+#include <librdkafka/rdkafkacpp.h>
+
+const int TIMEOUT_MS = 500;
+
+/**
+ * This class is only needed for testing purposes.
+ */
+class KafkaInputStream: public MsgInputStream {
+private:
+  std::string topic;
+  std::string brokers;
+  std::string group_id;
+
+  RdKafka::Conf *conf;
+  RdKafka::KafkaConsumer *consumer;
+public:
+  KafkaInputStream(std::string topic, std::string brokers, std::string group_id);
+  virtual ~KafkaInputStream();
+  // prevent copy construction and assignment
+  KafkaInputStream(const KafkaInputStream &other) = delete;
+  KafkaInputStream& operator=(const KafkaInputStream &other) = delete;
+
+  virtual int open() override;
+  virtual void close() override;
+  virtual int recv(std::string &next_msg) override;
+};
+
+#endif /* IO_KAFKA_INPUT_STREAM_H_ */
